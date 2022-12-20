6be346515d1bfdeb423f62650ec2e17767862d4c
hisho <hisho.web@gmail.com>
2022-12-20 19:33:20 +0900

    🚨 fixed lint error

--- src/constant/meta.ts
+++ src/constant/meta.ts
@@ -1,2 +1,3 @@
-export const NEXT_PUBLIC_DESCRIPTION = process.env.NEXT_PUBLIC_DESCRIPTION ?? ''
-export const NEXT_PUBLIC_TITLE = process.env.NEXT_PUBLIC_TITLE ?? ''
+export const NEXT_PUBLIC_DESCRIPTION =
+  process.env['NEXT_PUBLIC_DESCRIPTION'] ?? ''
+export const NEXT_PUBLIC_TITLE = process.env['NEXT_PUBLIC_TITLE'] ?? ''
