8970913481c34077d890a9b51b70ad65d0f68c99
hisho <hisho.web@gmail.com>
2022-12-22 04:39:08 +0900

    ✨ 都道府県から総人口のグラフを表示するページの作成

--- src/pages/content.tsx
+++ src/pages/content.tsx
@@ -1,3 +1,79 @@
+import { LineChart } from '@src/component/Chart/LineChart/LineChart'
+import { fetchTotalPopulation } from '@src/feature/population/getTotalPopulation/getTotalPopulation'
+import { PrefectureCheckboxGroup } from '@src/feature/prefecture/PrefectureCheckboxGroup/PrefectureCheckboxGroup'
+import type { TotalPopulation } from '@src/model/population/totalPopulation'
+import { useMemo, useState } from 'react'
+import { isArrayNotEmpty } from 'typesafe-utils'
+
 export const Content = () => {
-  return <div>Next.js</div>
+  const [state, setState] = useState<
+    Map<
+      string,
+      {
+        name: string
+        data: TotalPopulation[]
+      }
+    >
+  >(new Map())
+
+  const minYear = useMemo(() => {
+    const years = Array.from(state).flatMap(([, value]) =>
+      value.data.map((v) => v.year)
+    )
+    return isArrayNotEmpty(years) ? Math.min(...years) : 0
+  }, [state])
+
+  const lineChartData = Array.from(state).map(([, value]) => ({
+    name: value.name,
+    values: value.data.map((data) => data.value),
+  }))
+
+  const setTotalPopulation = async (prefecture: {
+    name: string
+    code: number
+  }) => {
+    const data = await fetchTotalPopulation({
+      prefectureCode: prefecture.code,
+    })
+    setState((prevState) => {
+      const newState = new Map(prevState)
+      newState.set(prefecture.name, {
+        data,
+        name: prefecture.name,
+      })
+      return newState
+    })
+  }
+
+  const deleteTotalPopulation = (prefecture: {
+    name: string
+    code: number
+  }) => {
+    setState((prevState) => {
+      const newState = new Map(prevState)
+      newState.delete(prefecture.name)
+      return newState
+    })
+  }
+
+  const handleChangeCheckbox = async ({
+    isChecked,
+    prefecture,
+  }: {
+    isChecked: boolean
+    prefecture: { name: string; code: number }
+  }) => {
+    if (isChecked) {
+      await setTotalPopulation(prefecture)
+    } else {
+      deleteTotalPopulation(prefecture)
+    }
+  }
+
+  return (
+    <div>
+      <PrefectureCheckboxGroup onChange={handleChangeCheckbox} />
+      <LineChart title={'総人口'} data={lineChartData} pointStart={minYear} />
+    </div>
+  )
 }
